0cda7b120e97c6ca8160a99f91a7a4d2eaa861f4
+ <+>
2021-10-01 12:48:23 -0700

    added controller

---
+++ demo/app/controllers/starships_controller.rb
@@ -0,0 +1,16 @@
+class StarshipsController < ApplicationController
+
+  get '/starships' do
+    Starship.all.to_json
+  end
+
+  post '/starships' do
+    starship = Starship.create(
+      name: params[:name],
+      model: params[:model],
+      color: params[:color]
+    )
+    starship.to_json
+  end
+
+end
